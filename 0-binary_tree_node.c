f61981b118a784967e1751fd92f77f92ccaf353b
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-04-07 16:07:27 +1000

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,14 +1,20 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node- creates a new node with a value
+ * @parent: the parent node to our new node
+ * @value: the number we want to pass to the node
+ * Return: the created node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
-	
+
 	node = malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
 		return (NULL);
-
+	
 	node->n = value;
 	node->parent = parent;
 	node->right = NULL;

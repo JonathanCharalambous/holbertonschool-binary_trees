e6910c8a2512c82f19d0ea104314896acbc7009c
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-08 06:49:10 +0000

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,13 +3,13 @@
 /**
  * binary_tree_height- determines the depth/height of a binary tree
  * @tree: the tree to check
- * Return: +1 layer to the tree side until the root is reached 
+ * Return: +1 layer to the tree side until the root is reached
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lheight = 0;
 	size_t rheight = 0;
-	
+
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 

c2a964d23092bdb1c4e71d239c2a994f96522fca
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-09 04:57:34 +0000

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,30 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height- determines the depth/height of a binary tree
+ * @tree: the tree to check
+ * Return: +1 layer to the tree side until the root is reached
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+        size_t lheight = 0;
+        size_t rheight = 0;
+
+        if (tree == NULL)
+                return (0);
+
+        lheight = binary_tree_height(tree->left);
+        rheight = binary_tree_height(tree->right);
+
+        if (lheight > rheight)
+                return (lheight + 1);
+        else
+                return (rheight + 1);
+
+}
+
+
+/**
  * binary_tree_balance- finds the balance of a binary tree
  * @tree: the tree to check
  * Return: the balance of the tree
